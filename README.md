9b5687ac5e0b7d850e5fd803fec88cb36ce0fc38
CitizenOneX <17427385+CitizenOneX@users.noreply.github.com>
2024-08-14 08:15:40 +1000

    initial flutter create

---
+++ README.md
@@ -0,0 +1,3 @@
+# wiki_frame
+
+Listens for a search phrase from the user, queries Wikipedia and returns an extract for display in Frame
---
+++ android/app/src/main/kotlin/com/example/wiki_frame/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.wiki_frame
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
